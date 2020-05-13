a4b2beba4e286d3aca2fe1397ac043bd3444feb7
lijie <lijietest@sohu.com>
2020-05-13 15:01:54 +0800

    latest

---
+++ Outlook_extend/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Outlook_extend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAcessEmail = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAcessEmail
+            // 
+            this.btnAcessEmail.Location = new System.Drawing.Point(-2, 287);
+            this.btnAcessEmail.Name = "btnAcessEmail";
+            this.btnAcessEmail.Size = new System.Drawing.Size(63, 59);
+            this.btnAcessEmail.TabIndex = 0;
+            this.btnAcessEmail.Text = "ViewSentItem";
+            this.btnAcessEmail.UseVisualStyleBackColor = true;
+            this.btnAcessEmail.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.richTextBox1.Location = new System.Drawing.Point(219, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(581, 450);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(-2, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Subject filter(Daily reprot from xxx)";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(-2, 206);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(200, 55);
+            this.richTextBox4.TabIndex = 8;
+            this.richTextBox4.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-2, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 56);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Send to my mailbox";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(-2, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 64);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Add Time";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(2, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(196, 78);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "ID";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "DateRange";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnAcessEmail);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "HappyWork, Happy life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAcessEmail;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ Outlook_extend/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Outlook;
+
+namespace Outlook_extend
+{
+    public partial class Form1 : Form
+    {
+        Microsoft.Office.Interop.Outlook.Application app = null;
+        Microsoft.Office.Interop.Outlook._NameSpace ns = null;
+        Microsoft.Office.Interop.Outlook.Items sendEmailItems = null;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            itemMapInfo();
+
+            
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //create map key:createTime-----value:System.__ComObject
+
+
+        public SortedDictionary<DateTime, object> itemMapInfo() {
+
+            SortedDictionary<DateTime, object> tempdic = new SortedDictionary<DateTime, object>();
+
+            app = new Microsoft.Office.Interop.Outlook.Application();
+            ns = app.GetNamespace("MAPI");
+            ns.Logon("emailadress", "pwd", false, false);
+            sendEmailItems = ns.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderSentMail).Items;
+
+
+            //sendEmailItems = sendEmailItems.Restrict("CreationTime");
+            //sendEmailItems = sendEmailItems.Sort("CreationTime", OlSortOrder.olDescending);
+            richTextBox1.AppendText("\n" + sendEmailItems.Count);
+            try
+            {
+
+                foreach (object mail in sendEmailItems)
+                {
+                    if ((mail as Microsoft.Office.Interop.Outlook.MailItem) != null)
+                    {
+
+                        //richTextBox1.AppendText( );
+                        //richTextBox1.AppendText("Subject: " + (mail as Microsoft.Office.Interop.Outlook.MailItem).Subject);
+                        //richTextBox1.AppendText("CreationTime: " + (mail as Microsoft.Office.Interop.Outlook.MailItem).CreationTime);
+                        //richTextBox1.AppendText("HTMLBody: " + (mail as Microsoft.Office.Interop.Outlook.MailItem).HTMLBody);
+                        tempdic.Add((mail as Microsoft.Office.Interop.Outlook.MailItem).CreationTime, (mail as Microsoft.Office.Interop.Outlook.MailItem).Subject + "\n" + (mail as Microsoft.Office.Interop.Outlook.MailItem).Body) ;
+                        
+
+                    }
+                        
+       
+                    
+                }
+
+                tempdic.OrderBy(KeyValuePair => KeyValuePair.Key);
+                foreach (KeyValuePair<DateTime, object> keyValuePair in tempdic)
+                {
+                    if(DateTime.Compare(keyValuePair.Key, Convert.ToDateTime(dataGridView1.Rows[0].Cells[1].Value)) >0 && DateTime.Compare(keyValuePair.Key, Convert.ToDateTime(dataGridView1.Rows[1].Cells[1].Value))<0)
+
+                    richTextBox1.AppendText("\n" + keyValuePair.Key);
+                    richTextBox1.AppendText("\n" + keyValuePair.Value);
+
+
+                }
+
+
+                //String path = System.Envrionment.currentDictionery;
+                //Path.Combine(Environment.CurrentDirectory, @"topologyInfo.xml"
+                //foreach (object mail in tempdic)
+                //{
+                //    if ((mail as Microsoft.Office.Interop.Outlook.MailItem) != null)
+                //    {
+
+                //        (mail as Microsoft.Office.Interop.Outlook.MailItem).Saveas(path,5);
+
+
+                //    }
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+            catch (System.Runtime.InteropServices.COMException ex)
+            {
+                richTextBox1.AppendText(ex.ToString());
+            }
+            finally
+            {
+                ns = null;
+                app = null;
+            }
+
+            return tempdic;
+        }
+
+
+
+        //Cumulative method to get list item in weekly report
+        /*
+            Day01 {issue, progress, teststatus, plan}
+            Day01 {issue2, progress2, teststatus2, plan2}
+            .
+            .
+            .
+            Weekly {issue + issue2+..., progress+progress2+..., plan+plan2+...}
+             */
+
+        public List<string> cumulativefunc(SortedDictionary<string, string> cumuDic) {
+
+
+            List<string> temp = new List<string>();
+            temp = null;
+            
+            //ensure data add from sorted dictionnary
+            /* day01--day02--day03--...*/
+            foreach (KeyValuePair<string, string> keyPare in cumuDic) {
+
+                temp.Add(keyPare.Key);
+             
+                if (!temp.Contains(keyPare.Key)) {
+
+                    temp.Add(keyPare + "---------Resolved");
+                    }
+
+                temp.Add(keyPare.Key);
+                
+                
+            
+            }
+
+
+            return temp;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+           
+
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            int MaxRows = 2;
+
+            
+
+            if (dataGridView1.Rows.Count < MaxRows)
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.AllowUserToAddRows = true;
+                dataGridView1.Rows[n].Cells[0].Value = (n + 1).ToString();
+                dataGridView1.Rows[n].Cells[1].Value = dateTimePicker1.Value.ToString("MM-dd-yyyy");
+                
+            }
+
+            
+            else
+            {
+                dataGridView1.AllowUserToAddRows = false;
+            }
+
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+    }
+
+
+
+    public class myMail {
+
+        public DateTime myDateTime { get; }
+        public string mySubject { get; }
+        public string myBody { get; }
+        public myMail(DateTime mytime, string subject, string body) {
+            myDateTime = mytime;
+            mySubject = subject;
+            myBody = body; 
+
+        }
+
+        public void sortTime() {
+
+        
+        }
+        
+
+    
+    }
+}
